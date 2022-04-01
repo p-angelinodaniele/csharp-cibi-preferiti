0c01806442a093450c71776e0880a818b3135f98
P.angelino <p.angelinodaniele@gmail.com>
2022-04-01 15:52:54 +0200

    Correzione

--- Program.cs
+++ Program.cs
@@ -1,14 +1,32 @@
 ﻿string[] cibiPreferiti = { "Pizza", "Hamburger", "Pasta", "Patatine", "Pollo", "Cheesecake", "Crepes", "Latte",};
+string[] cibiPreferitiClassifica = { "Pizza", "Hamburger", "Patatine", "Pollo", "Cheesecake", "latte", "Crepes", "Pasta"};
 
-Console.WriteLine("La lunghezza dell'array è: " + cibiPreferiti.Length);
-Console.WriteLine(" 1 " + cibiPreferiti[0]);
-Console.WriteLine(" 2 " + cibiPreferiti[1]);
-Console.WriteLine(" 3 " + cibiPreferiti[3]);
-Console.WriteLine(" 4 " + cibiPreferiti[4]);
-Console.WriteLine(" 5 " + cibiPreferiti[5]);
-Console.WriteLine(" 6 " + cibiPreferiti[7]);
-Console.WriteLine(" 7 " + cibiPreferiti[6]);
-Console.WriteLine(" 8 " + cibiPreferiti[2]);
-Console.WriteLine("Il mio cibo preferito è la " + cibiPreferiti[0]);
-Console.WriteLine("Il cibo che preferisco di meno è la " + cibiPreferiti[2]);
 
+Console.WriteLine("La lunghezza dell'array è: " + cibiPreferitiClassifica.Length);
+
+
+var contatore = 1;
+for (int i = 0; i < cibiPreferitiClassifica.Length; i++)
+{
+    Console.WriteLine("Al " + contatore + " Posto " + cibiPreferitiClassifica[i]);
+    contatore++;
+}
+
+Console.WriteLine("Al primo posto c'è: " + cibiPreferitiClassifica[0]);
+//Console.WriteLine("All'ultimo posto c'è: " + cibiPreferitiClassifica[7]);
+
+
+var lunghezzaArray = cibiPreferitiClassifica.Length;
+var modulo = lunghezzaArray % 2;
+if (modulo == 0)
+{
+    var primoElemento = lunghezzaArray / 2;
+    var secondoElemento = primoElemento - 1;
+    Console.WriteLine("A metà classifica troviamo " + cibiPreferitiClassifica[secondoElemento] + " e " + cibiPreferitiClassifica[primoElemento]);
+    
+
+}else if (modulo == 1)
+{
+    lunghezzaArray /= 2;
+    Console.WriteLine("A metà classifica troviamo " + cibiPreferitiClassifica[lunghezzaArray]);
+}
