9d026ee33ab9d703a3c63386e829cc1ee7030ec1
P.angelino <p.angelinodaniele@gmail.com>
2022-04-01 15:05:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿string[] cibiPreferiti = { "Pizza", "Hamburger", "Pasta", "Patatine", "Pollo", "Cheesecake", "Crepes", "Latte",};
+
+Console.WriteLine("La lunghezza dell'array è: " + cibiPreferiti.Length);
+Console.WriteLine(" 1 " + cibiPreferiti[0]);
+Console.WriteLine(" 2 " + cibiPreferiti[1]);
+Console.WriteLine(" 3 " + cibiPreferiti[3]);
+Console.WriteLine(" 4 " + cibiPreferiti[4]);
+Console.WriteLine(" 5 " + cibiPreferiti[5]);
+Console.WriteLine(" 6 " + cibiPreferiti[7]);
+Console.WriteLine(" 7 " + cibiPreferiti[6]);
+Console.WriteLine(" 8 " + cibiPreferiti[2]);
+Console.WriteLine("Il mio cibo preferito è la " + cibiPreferiti[0]);
+Console.WriteLine("Il cibo che preferisco di meno è la " + cibiPreferiti[2]);
+
